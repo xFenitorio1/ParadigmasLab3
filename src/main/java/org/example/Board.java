904c9a80c7832e0034e20c8997308513d65f83f8
Gerardo Perez <gerardo.perez@usach.cl>
2024-12-20 22:29:08 -0300

    RF sePuedeJugar

--- src/main/java/org/example/Board.java
+++ src/main/java/org/example/Board.java
@@ -1,50 +1,61 @@
 package org.example;
-
 import java.util.ArrayList;
 import java.util.List;
 
-
 public class Board {
-    private int filas;
-    private int columnas;
-    private List<List<Character>> tablero;
+    private int filas; // Número de filas del tablero
+    private int columnas; // Número de columnas del tablero
+    private List<List<Character>> tablero; // Tablero representado como lista de listas
 
     public Board(int filas, int columnas) {
         this.filas = filas;
         this.columnas = columnas;
-        tablero = new ArrayList<>();
+        tablero = new ArrayList<>(columnas);
 
-        for (int i = 0; i < filas; i++) {
-            List<Character> row = new ArrayList<>();
-            for (int j = 0; j < columnas; j++) {
-                row.add('0');
+
+        for (int col = 0; col < columnas; col++) {
+            List<Character> columna = new ArrayList<>(filas);
+            for (int fila = 0; fila < filas; fila++) {
+                columna.add('0');
             }
-            tablero.add(row);
+            tablero.add(columna);
         }
     }
 
-    public int getFilas() {
-        return filas;
+    public void printBoard() {
+        for (int fila = 0; fila < filas; fila++) {
+            for (int col = 0; col < columnas; col++) {
+                System.out.print("| " + tablero.get(col).get(fila) + " ");
+            }
+            System.out.println("|");
+        }
+        System.out.print("  ");
+        for (int col = 1; col <= columnas; col++) {
+            System.out.print(col + " ");
+        }
+        System.out.println();
+    }
+    //La idea es usar esta funcion despues de que cada pieza se coloque
+    //lo mismo con check vertical, horizontal y diagonal.
+    public boolean canPlay() {
+        for (List<Character> columna : tablero) {
+            if (columna.get(0) == '0') { // Verificar si la primera fila está vacía en alguna columna
+                return true;
+            }
+        }
+        return false;
     }
 
-    public void setFilas(int filas) {
-        this.filas = filas;
+    // Getters
+    public int getFilas() {
+        return filas;
     }
 
     public int getColumnas() {
         return columnas;
     }
 
-    public void setColumnas(int columnas) {
-        this.columnas = columnas;
-    }
-
-    public void printBoard() {
-        for (List<Character> row : tablero) {
-            for (Character cell : row) {
-                System.out.print("| " + cell + " ");
-            }
-            System.out.println("|");
-        }
+    public List<List<Character>> getTablero() {
+        return tablero;
     }
 }
