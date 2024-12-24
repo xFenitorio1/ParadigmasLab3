f22f7427763dbe09bfbc9074d5fbba5248ededdf
Gerardo Perez <gerardo.perez@usach.cl>
2024-12-24 15:52:19 -0300

    esEmpate

--- src/main/java/org/example/Game.java
+++ src/main/java/org/example/Game.java
@@ -4,22 +4,20 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class Game {
+public class Game implements GameInterface {
     private Board board;
     private Player player1;
     private Player player2;
     private Player currentPlayer;
-    private int fichasPorJugador;
     public List<String> historial;
     private Piece colorPlayer1;
     private Piece colorPlayer2;
 
-    public Game(int filas, int columnas, int fichasPorJugador, Player player1, Player player2){
+    public Game(int filas, int columnas, int fichasPorJugador, Player player1, Player player2) {
         this.board = new Board(filas, columnas);
         this.player1 = player1;
         this.player2 = player2;
         this.currentPlayer = player1;
-        this.fichasPorJugador = fichasPorJugador;
         this.historial = new ArrayList<>();
         this.colorPlayer1 = new Piece(player1.getColor());
         this.colorPlayer2 = new Piece(player2.getColor());
@@ -33,5 +31,16 @@ public class Game {
         for (int i = 0; i < historial.size(); i++) {
             System.out.println((i + 1) + ". " + historial.get(i));
         }
+    }
+
+    public boolean esEmpate() {
+        // Verificar si el tablero tiene posiciones disponibles
+        boolean tableroLleno = !board.canPlay();
+
+
+        boolean sinFichas = (player1.getRemainingPieces() == 0 && player2.getRemainingPieces() == 0);
+
+
+        return tableroLleno || sinFichas;
     }
 }
